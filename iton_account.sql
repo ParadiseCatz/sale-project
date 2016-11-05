705e6ce90199e28475e2e4fa9ac22ab411e65350
joshua.salimin <13514001@std.stei.itb.ac.id>
2016-11-05 17:21:25 +0700

    menambahkan 2 basis data yang telah dipisahkan

---
+++ iton_account.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2016 at 11:20 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iton_account`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pelanggan`
+--
+
+CREATE TABLE `data_pelanggan` (
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `full_address` varchar(100) NOT NULL,
+  `postal_code` int(32) NOT NULL,
+  `phone_number` bigint(64) NOT NULL,
+  `id` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_pelanggan`
+--
+
+INSERT INTO `data_pelanggan` (`username`, `email`, `password`, `full_name`, `full_address`, `postal_code`, `phone_number`, `id`) VALUES
+('asd', 'asd@gmail.com', 'asd', 'asd', 'tubagus ismail ', 12345, 81122445566, 2),
+('root', 'root@gmail.com', 'root', 'root', 'Dago 12345', 12345, 81122334455, 1),
+('wil', 'wil@gmail.com', 'asd', 'william', 'akakaak', 12345, 8111111115, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `email` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`email`, `username`, `password`, `id`) VALUES
+('asd@gmail.com', 'asd', 'asd', 2),
+('root@gmail.com', 'root', 'root', 1),
+('wil@gmail.com', 'wil', 'asd', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_pelanggan`
+--
+ALTER TABLE `data_pelanggan`
+  ADD PRIMARY KEY (`username`,`email`,`password`,`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`email`,`username`,`password`,`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_pelanggan`
+--
+ALTER TABLE `data_pelanggan`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ iton_marketplace.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2016 at 11:20 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iton_marketplace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(32) NOT NULL,
+  `id_penjual` int(32) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` bigint(64) NOT NULL,
+  `deskripsi` varchar(1000) NOT NULL,
+  `nama_foto` varchar(100) NOT NULL,
+  `waktu_ditambahkan` datetime NOT NULL,
+  `jumlah_like` int(32) NOT NULL,
+  `jumlah_dibeli` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `id_penjual`, `nama_barang`, `harga`, `deskripsi`, `nama_foto`, `waktu_ditambahkan`, `jumlah_like`, `jumlah_dibeli`) VALUES
+(40, 1, 'PS4', 4500000, 'PS4 Console master race', 'image\\57f79dc3b3457.jpg', '2016-10-07 20:06:11', 1, 0),
+(48, 1, 'ps5', 12350000, 'asdlasjdaskl', 'image\\58073ddb9077a.png', '2016-10-19 16:33:15', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(32) NOT NULL,
+  `id_pembeli` int(32) NOT NULL,
+  `quantity` int(32) NOT NULL,
+  `cosignee` varchar(50) NOT NULL,
+  `full_address` varchar(100) NOT NULL,
+  `postal_code` int(32) NOT NULL,
+  `phone_number` bigint(64) NOT NULL,
+  `creditcard_number` bigint(32) NOT NULL,
+  `creeditcard_verification` int(4) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga_barang` bigint(64) NOT NULL,
+  `path_foto` varchar(100) NOT NULL,
+  `id_penjual` int(32) NOT NULL,
+  `waktu_transaksi` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `id_pembeli`, `quantity`, `cosignee`, `full_address`, `postal_code`, `phone_number`, `creditcard_number`, `creeditcard_verification`, `nama_barang`, `harga_barang`, `path_foto`, `id_penjual`, `waktu_transaksi`) VALUES
+(3, 2, 1, 'asd', 'tubagus ismail ', 12345, 81122445566, 123123123123, 321, 'XBOX', 5000000, 'image\\57f79f2c4e71b.png', 1, '2016-10-07 15:13:30'),
+(4, 3, 3, 'william', 'akakaak', 12345, 8111111115, 123456789999, 123, 'XBOX', 5000000, 'image\\57f79f2c4e71b.png', 1, '2016-10-19 11:30:13'),
+(5, 2, 1, 'asd', 'tubagus ismail ', 12345, 81122445566, 123456789000, 123, 'ps5', 12350000, 'image\\58073ddb9077a.png', 1, '2016-10-19 11:38:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_liked`
+--
+
+CREATE TABLE `user_liked` (
+  `id_user` int(32) NOT NULL,
+  `id_barang` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_liked`
+--
+
+INSERT INTO `user_liked` (`id_user`, `id_barang`) VALUES
+(2, 13),
+(2, 14),
+(2, 40),
+(2, 46),
+(3, 46);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`,`id_penjual`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_liked`
+--
+ALTER TABLE `user_liked`
+  ADD PRIMARY KEY (`id_user`,`id_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
