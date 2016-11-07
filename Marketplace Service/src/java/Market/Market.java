a82c706724d15b865e328b2aaa9ac7b3cbc83281
StefanusAgus <stefanusagusharyono@gmail.com>
2016-11-07 23:34:32 +0700

    Create getCatalog list (SQL Connection error)

---
+++ Marketplace Service/src/java/Market/Market.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Market;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+
+/**
+ *
+ * @author ASUS
+ */
+@WebService(serviceName = "Market")
+public class Market {
+    
+    //Connect to database
+    Connection conn=getConnection();
+    
+    public static Connection getConnection(){
+        //membuka koneksi ke database marketplace
+        Connection conn = null;
+        String URL="jdbc:mysql://localhost:3306/iton_marketplace";
+        String USER="root";
+        String PASS="";
+        try {
+            conn=DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            Logger.getLogger(Market.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conn;
+    }
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "listCatalog")
+    @WebResult(name="Produk")
+    public ArrayList<Produk> listCatalog(@WebParam(name = "userID") int userID){
+        //TODO write your implementation code here:
+        //TODO write your implementation code here:
+        //TODO write your implementation code here:
+        ArrayList<Produk> daftarCatalog=new ArrayList<Produk>();
+        try {
+            Statement sqlStatement=conn.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Market.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query="SELECT * FROM barang where id_penjual<>? ORDER BY waktu_ditambahkan DESC";
+        PreparedStatement dbStatement = null;
+        try {
+            dbStatement = conn.prepareStatement(query);
+            dbStatement.setInt(1, userID);
+        } catch (SQLException ex) {
+            Logger.getLogger(Market.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ResultSet result = null;
+        try {
+            result = dbStatement.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(Market.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        int i=0;
+        try {
+            while (result.next()){
+                daftarCatalog.add(new Produk(result.getInt("id"),result.getInt("id_penjual"),
+                        result.getString("username"), result.getString("nama_barang"),result.getLong("harga"),
+                        result.getString("deskripsi"),result.getString("nama_foto"),result.getString("waktu_ditambahkan"),
+                        result.getInt("jumlah_like"),result.getInt("jumlah_dibeli")));
+                ++i;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Market.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return daftarCatalog;
+        
+    }
+}
