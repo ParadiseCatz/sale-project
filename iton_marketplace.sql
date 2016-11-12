17375044d4dc56f34b1c7e7a17e234f2141ed5d9
joshua.salimin <13514001@std.stei.itb.ac.id>
2016-11-12 18:39:11 +0700

    add method sales pada Market.java

--- iton_marketplace.sql
+++ iton_marketplace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2016 at 11:27 AM
+-- Generation Time: Nov 12, 2016 at 11:37 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -35,7 +35,7 @@ CREATE TABLE `transaction` (
   `postal_code` int(32) NOT NULL,
   `phone_number` bigint(64) NOT NULL,
   `creditcard_number` bigint(32) NOT NULL,
-  `creeditcard_verification` int(4) NOT NULL,
+  `creditcard_verification` int(4) NOT NULL,
   `nama_barang` varchar(50) NOT NULL,
   `harga_barang` bigint(64) NOT NULL,
   `foto` mediumtext NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE `transaction` (
 -- Dumping data for table `transaction`
 --
 
-INSERT INTO `transaction` (`id`, `id_pembeli`, `quantity`, `cosignee`, `full_address`, `postal_code`, `phone_number`, `creditcard_number`, `creeditcard_verification`, `nama_barang`, `harga_barang`, `foto`, `id_penjual`, `waktu_transaksi`) VALUES
+INSERT INTO `transaction` (`id`, `id_pembeli`, `quantity`, `cosignee`, `full_address`, `postal_code`, `phone_number`, `creditcard_number`, `creditcard_verification`, `nama_barang`, `harga_barang`, `foto`, `id_penjual`, `waktu_transaksi`) VALUES
 (3, 2, 1, 'asd', 'tubagus ismail ', 12345, 81122445566, 123123123123, 321, 'XBOX', 5000000, 'image\\57f79f2c4e71b.png', 1, '2016-10-07 15:13:30'),
 (4, 3, 3, 'william', 'akakaak', 12345, 8111111115, 123456789999, 123, 'XBOX', 5000000, 'image\\57f79f2c4e71b.png', 1, '2016-10-19 11:30:13'),
 (5, 2, 1, 'asd', 'tubagus ismail ', 12345, 81122445566, 123456789000, 123, 'ps5', 12350000, 'image\\58073ddb9077a.png', 1, '2016-10-19 11:38:33');
